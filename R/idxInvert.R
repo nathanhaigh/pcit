6dccbd6b31124d37e1668d3d81c0e677a28e3109
Nathan Watson-Haigh <nathan.watson-haigh@sahmri.com>
2020-12-22 12:14:09 +1030

    More robust input checking

--- R/idxInvert.R
+++ R/idxInvert.R
@@ -1,15 +1,21 @@
 idxInvert <- function(m, idx) {
 	if (methods::is(m, "numeric")) {
-		nNodes <- m
+		if (length(m)==1) {
+			nNodes <- m
+		} else if (length(m)==2 & m[1]==m[2]) {
+			nNodes <- m[1]
+		} else {
+			stop("ERROR: when providing the size of a matrix using 'm', it must be a single numeric or the dimensions of a square matrix.")
+		}
+	} else if (length(unique(dim(m)))!=1) {
+		stop("ERROR: when providing the actual matrix using 'm', it must be square.")
 	} else {
 		nNodes <- tryCatch(
 			{
 				nrow(m)
 			},
 			error = function(cond) {
-				message("ERROR: argument 'm' must be a numeric OR an object on which nrow() can be performed.")
-				message(cond)
-				return(FALSE)
+				stop("ERROR: argument 'm' must be a numeric OR an object on which nrow() can be performed.")
 			}
 		)
 	}
