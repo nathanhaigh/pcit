545364804e2777ed1f42961a3cc8b60ea485b28d
Nathan Watson-Haigh <nathan.watson-haigh@sahmri.com>
2021-01-21 13:59:07 +1030

    Code tidy and support matrix without dimnames

--- R/getEdgeList.R
+++ R/getEdgeList.R
@@ -1,13 +1,52 @@
 getEdgeList <- function(m, rm.zero=TRUE) {
-	# TODO check if this returns the upper or lower triangle
+	nNodes <- unique(dim(m))
 	
-	from_idx <- vector(mode='integer', length=((nrow(m)^2)-nrow(m))/2)
-	to_idx <- vector(mode='integer', length=((nrow(m)^2)-nrow(m))/2)
-	weight <- vector(mode='numeric', length=((nrow(m)^2)-nrow(m))/2)
-	
-	result <- .Fortran('getedgelist', mat=as.single(m), nGenes=as.integer(nrow(m)), from_idx=as.integer(from_idx), to_idx=as.integer(to_idx), weight=as.single(weight))
-	
-	edgeList <- data.frame(From=rownames(m)[result$from_idx], To=colnames(m)[result$to_idx], Weight=as.vector(result$weight))
-	
-	return(edgeList[edgeList$Weight != 0,])
+	if (length(nNodes)!=1) {
+		stop("ERROR: 'm' must have the same number of rows as columns.")
+	}
+
+	from_idx <- vector(
+		mode   = 'integer',
+		length = ((nNodes^2)-nNodes) / 2
+	)
+	to_idx <- vector(
+		mode   = 'integer',
+		length = ((nNodes^2)-nNodes) / 2
+	)
+	weight <- vector(
+		mode   = 'numeric',
+		length = ((nNodes^2)-nNodes) / 2
+	)
+
+	result <- .Fortran(
+		'getedgelist',
+		mat      = as.single(m),
+		nGenes   = as.integer(nNodes),
+		from_idx = as.integer(from_idx),
+		to_idx   = as.integer(to_idx),
+		weight   = as.single(weight)
+	)
+
+	if (length(dimnames(m))==2) {
+		# Both row and column names provided
+		edgeList <- data.frame(
+			From   = rownames(m)[result$from_idx],
+			To     = colnames(m)[result$to_idx],
+			Weight = as.vector(result$weight)
+		)
+	} else if (length(dimnames(m))==0) {
+		edgeList <- data.frame(
+			From   = as.vector(result$from_idx),
+			To     = as.vector(result$to_idx),
+			Weight = as.vector(result$weight)
+		)
+	} else {
+		stop("ERROR: 'm' must have both col and row names or neither.")
+	}
+
+	if (rm.zero==TRUE) {
+		edgeList <- edgeList[edgeList$Weight != 0,]
+	}
+
+	return(edgeList)
 }
